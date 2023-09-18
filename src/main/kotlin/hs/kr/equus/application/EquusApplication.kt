4d81da0810f14fa9c4463f9268b3ccb465231ecd
seongwon <seongwon1290@dsm.hs.kr>
2023-09-18 22:23:39 +0900

    🎉 :: Create Project

---
+++ src/main/kotlin/hs/kr/equus/application/EquusApplication.kt
@@ -0,0 +1,11 @@
+package hs.kr.equus.application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EquusApplication
+
+fun main(args: Array<String>) {
+	runApplication<EquusApplication>(*args)
+}

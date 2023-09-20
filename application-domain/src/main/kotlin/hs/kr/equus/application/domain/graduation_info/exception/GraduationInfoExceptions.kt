38cd502f0d98da2061482bd7961ecaf97f7e3a63
seongwon <seongwon1290@dsm.hs.kr>
2023-09-20 14:45:50 +0900

    📑 :: GraduationInfoExceptions

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/graduation_info/exception/GraduationInfoExceptions.kt
@@ -0,0 +1,13 @@
+package hs.kr.equus.application.domain.graduation_info.exception
+
+import hs.kr.equus.application.global.exception.BusinessException
+
+sealed class GraduationInfoExceptions(
+    override val status: Int,
+    override val message: String
+) : BusinessException(status, message) {
+
+    companion object {
+
+    }
+}

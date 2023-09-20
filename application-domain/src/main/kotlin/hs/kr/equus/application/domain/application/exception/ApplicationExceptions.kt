5ff0220b968b642af21b4a1e74283389aee888f9
seongwon <seongwon1290@dsm.hs.kr>
2023-09-20 14:45:41 +0900

    📑 :: ApplicationExceptions

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/exception/ApplicationExceptions.kt
@@ -0,0 +1,13 @@
+package hs.kr.equus.application.domain.application.exception
+
+import hs.kr.equus.application.global.exception.BusinessException
+
+sealed class ApplicationExceptions(
+    override val status: Int,
+    override val message: String
+) : BusinessException(status, message) {
+
+    companion object {
+
+    }
+}

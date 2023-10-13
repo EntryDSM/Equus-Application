b301131378a995f4b8ccf2521a8fe06aa9e969ec
seongwon <seongwon1290@dsm.hs.kr>
2023-10-13 11:36:40 +0900

    style: Ktlint

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/UpdateIntroduceUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/UpdateIntroduceUseCase.kt
@@ -19,7 +19,7 @@ class UpdateIntroduceUseCase(
         commandApplicationPort.save(
             application.copy(
                 selfIntroduce = request.content,
-            )
+            ),
         )
     }
 }

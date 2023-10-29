c7a412576c84fa911add8846976aecaeb0e4755c
seongwon <seongwon1290@dsm.hs.kr>
2023-10-29 19:59:24 +0900

    refactor: 졸업전형 수정 로직 이전

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/request/UpdateApplicationTypeRequest.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/request/UpdateApplicationTypeRequest.kt
@@ -3,7 +3,7 @@ package hs.kr.equus.application.domain.application.usecase.dto.request
 import hs.kr.equus.application.domain.application.model.types.ApplicationRemark
 import hs.kr.equus.application.domain.application.model.types.ApplicationType
 
-data class UpdateTypeRequest(
+data class UpdateApplicationTypeRequest(
     val applicationType: ApplicationType,
     val applicationRemark: ApplicationRemark?,
     val isDaejeon: Boolean,
--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/request/UpdateGraduationTypeRequest.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/request/UpdateGraduationTypeRequest.kt
@@ -1,4 +1,4 @@
-package hs.kr.equus.application.domain.graduationInfo.usecase.dto.request
+package hs.kr.equus.application.domain.application.usecase.dto.request
 
 import hs.kr.equus.application.domain.application.model.types.EducationalStatus
 import java.time.LocalDate

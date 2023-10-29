0c29d0b64b011afa9774f6488ce79f07fa867337
seongwon <seongwon1290@dsm.hs.kr>
2023-10-30 08:38:03 +0900

    file: School

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/school/model/School.kt
@@ -0,0 +1,18 @@
+package hs.kr.equus.application.domain.school.model
+
+import hs.kr.equus.application.domain.school.exception.SchoolExceptions
+import hs.kr.equus.application.global.annotation.Aggregate
+
+@Aggregate
+data class School(
+    val code: String,
+    val name: String,
+    val tel: String,
+    val type: String,
+) {
+    init {
+        check(type == "중학교") {
+            throw SchoolExceptions.InvalidSchoolTypeException()
+        }
+    }
+}

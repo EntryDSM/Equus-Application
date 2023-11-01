4aa3d4b02a07feef5097e09ebeb825cead7469ca
seongwon <seongwon1290@dsm.hs.kr>
2023-11-01 17:18:55 +0900

    file: 도메인 포트를 상속하는 구현용 단일 포트

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/spi/ApplicationPort.kt
@@ -0,0 +1,5 @@
+package hs.kr.equus.application.domain.application.spi
+
+import hs.kr.equus.application.domain.graduationInfo.spi.GraduationInfoQueryApplicationPort
+
+interface ApplicationPort : CommandApplicationPort, QueryApplicationPort, GraduationInfoQueryApplicationPort
---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/school/spi/SchoolPort.kt
@@ -0,0 +1,5 @@
+package hs.kr.equus.application.domain.school.spi
+
+import hs.kr.equus.application.domain.graduationInfo.spi.GraduationInfoQuerySchoolPort
+
+interface SchoolPort : GraduationInfoQuerySchoolPort

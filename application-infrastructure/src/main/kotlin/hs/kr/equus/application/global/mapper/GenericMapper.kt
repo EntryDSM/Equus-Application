5725a95d93a2c293188b44f22c6e56bb004320f0
seongwon <seongwon1290@dsm.hs.kr>
2023-11-01 17:19:18 +0900

    file: GenericMapper

---
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/mapper/GenericMapper.kt
@@ -0,0 +1,10 @@
+package hs.kr.equus.application.global.mapper
+
+interface GenericMapper<E, D> {
+
+    fun toEntity(model: D): E
+
+    fun toDomain(entity: E?): D?
+
+    fun toDomainNotNull(entity: E): D
+}

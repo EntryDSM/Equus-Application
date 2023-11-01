579494a4df6252802d9918c09be1b85995058b79
seongwon <seongwon1290@dsm.hs.kr>
2023-11-01 17:20:13 +0900

    file: GraduationInfoEntity

---
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/graduationInfo/domain/entity/GraduationJpaEntity.kt
@@ -0,0 +1,29 @@
+package hs.kr.equus.application.domain.graduationInfo.domain.entity
+
+import hs.kr.equus.application.domain.graduationInfo.domain.entity.vo.StudentNumber
+import java.time.LocalDate
+import javax.persistence.Embedded
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Entity
+@Table(name = "tbl_graduation")
+class GraduationJpaEntity(
+    override val id: Long?,
+
+    override val graduateDate: LocalDate,
+
+    override val isProspectiveGraduate: Boolean,
+
+    override val receiptCode: Long,
+
+    @Embedded
+    val studentNumber: StudentNumber? = null,
+
+    val schoolCode: String? = null,
+) : GraduationInfoEntity(
+    id,
+    graduateDate,
+    isProspectiveGraduate,
+    receiptCode,
+)

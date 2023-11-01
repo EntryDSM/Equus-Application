3cae2b6bd63f50d885e01651079307215566e0ef
seongwon <seongwon1290@dsm.hs.kr>
2023-11-01 17:23:57 +0900

    file: ApplicationJpaRepository

---
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/application/domain/repository/ApplicationJpaRepository.kt
@@ -0,0 +1,9 @@
+package hs.kr.equus.application.domain.application.domain.repository
+
+import hs.kr.equus.application.domain.application.domain.entity.ApplicationJpaEntity
+import org.springframework.data.repository.CrudRepository
+import java.util.UUID
+
+interface ApplicationJpaRepository : CrudRepository<ApplicationJpaEntity, Long> {
+    fun findByUserId(userId: UUID): ApplicationJpaEntity?
+}

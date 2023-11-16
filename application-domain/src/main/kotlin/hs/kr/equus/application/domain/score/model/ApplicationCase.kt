8b16a07157d8b91dfd9bb3123e1d9455fa0eadd3
seongwon <seongwon1290@dsm.hs.kr>
2023-11-16 15:02:40 +0900

    file: Score Models

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/model/ApplicationCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/model/ApplicationCase.kt
@@ -1,9 +1,22 @@
 package hs.kr.equus.application.domain.score.model
 
-import java.util.UUID
+import java.math.BigDecimal
+import java.util.*
 
 abstract class ApplicationCase(
     receiptCode: Long,
 ) {
-    val id: UUID = UUID(0, 0)
+    val id: Long = 0
+    companion object {
+        const val MAX_ATTENDANCE_SCORE = 15
+        val COMMON_GRADE_RATE: BigDecimal = BigDecimal.valueOf(1.75)
+    }
+
+    abstract fun calculateVolunteerScore(): BigDecimal
+
+    abstract fun calculateAttendanceScore(): Int
+
+    abstract fun calculateGradeScores(): Array<BigDecimal>
+
+    abstract fun calculateTotalGradeScore(isCommon: Boolean): BigDecimal
 }
--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/model/QualificationCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/model/QualificationCase.kt
@@ -2,9 +2,34 @@ package hs.kr.equus.application.domain.score.model
 
 import hs.kr.equus.application.global.annotation.Aggregate
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 @Aggregate
 data class QualificationCase(
     val averageScore: BigDecimal,
     val receiptCode: Long,
-) : ApplicationCase(receiptCode)
+) : ApplicationCase(receiptCode) {
+    override fun calculateVolunteerScore(): BigDecimal {
+        return (averageScore - BigDecimal(40))
+            .divide(BigDecimal(4), 3, RoundingMode.HALF_UP)
+    }
+
+    override fun calculateAttendanceScore(): Int {
+        return MAX_ATTENDANCE_SCORE
+    }
+
+    override fun calculateGradeScores(): Array<BigDecimal> {
+        return arrayOf(BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO)
+    }
+
+    override fun calculateTotalGradeScore(isCommon: Boolean): BigDecimal {
+        val totalGradeScore =
+            ((averageScore - BigDecimal(50)) * BigDecimal.valueOf(1.6))
+                .setScale(3, RoundingMode.HALF_UP)
+
+        return if (isCommon) {
+            (totalGradeScore * (COMMON_GRADE_RATE))
+                .setScale(3, RoundingMode.HALF_UP)
+        } else totalGradeScore
+    }
+}

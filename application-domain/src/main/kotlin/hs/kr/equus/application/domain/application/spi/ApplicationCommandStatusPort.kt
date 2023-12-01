cd3fa91a09995e9c42d537ae0b53308ecb51e5e4
seongwon <seongwon1290@dsm.hs.kr>
2023-12-01 16:23:52 +0900

    file: delete classes

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/spi/ApplicationCommandStatusPort.kt
+++
@@ -1,7 +0,0 @@
-package hs.kr.equus.application.domain.application.spi
-
-import hs.kr.equus.application.domain.status.model.Status
-
-interface ApplicationCommandStatusPort {
-    fun save(status: Status) // todo message send라서 반환 없음
-}

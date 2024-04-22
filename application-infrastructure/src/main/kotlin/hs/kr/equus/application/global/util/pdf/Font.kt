3dee170b46306239398cf4eb68b2a13493a80bfd
도경 <dokyungkim2926@gmail.com>
2024-04-22 19:47:07 +0900

    add :: font

---
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/util/pdf/Font.kt
@@ -0,0 +1,10 @@
+package hs.kr.equus.application.global.util.pdf
+
+object Font {
+    val fonts = listOf(
+        "KoPubWorld Dotum Light.ttf",
+        "KoPubWorld Dotum Bold.ttf",
+        "KoPubWorld Dotum Medium.ttf",
+        "DejaVuSans.ttf"
+    )
+}

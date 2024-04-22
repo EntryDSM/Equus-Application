2b63b2d642ac4df1ccd96368c09e69e16446358a
도경 <dokyungkim2926@gmail.com>
2024-04-22 20:04:28 +0900

    add :: ConverterPropertiesCreator

---
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/util/pdf/ConverterPropertiesCreator.kt
@@ -0,0 +1,32 @@
+package hs.kr.equus.application.global.util.pdf
+
+import com.itextpdf.html2pdf.ConverterProperties
+import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider
+import com.itextpdf.io.font.FontProgramFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.io.IOException
+
+@Component
+class ConverterPropertiesCreator {
+
+    @Value("\${rolls-royce.font.path}")
+    private lateinit var fontPath: String
+
+    fun createConverterProperties(): ConverterProperties {
+        val properties = ConverterProperties()
+        val fontProvider = DefaultFontProvider(false, false, false)
+
+        Font.fonts.forEach { font ->
+            try {
+                val fontProgram = FontProgramFactory.createFont("$fontPath$font")
+                fontProvider.addFont(fontProgram)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        properties.setFontProvider(fontProvider)
+        return properties
+    }
+}

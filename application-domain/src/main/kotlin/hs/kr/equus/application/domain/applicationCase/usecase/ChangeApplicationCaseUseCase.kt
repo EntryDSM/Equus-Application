d8ffe2e553fe0857ecb72ea8d51a69f712d362c0
도경 <dokyungkim2926@gmail.com>
2024-05-13 08:32:27 +0900

    ktlint

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/applicationCase/usecase/ChangeApplicationCaseUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/applicationCase/usecase/ChangeApplicationCaseUseCase.kt
@@ -28,6 +28,6 @@ class ChangeApplicationCaseUseCase(
             application.educationalStatus
         )
 
-            commandApplicationCasePort.save(applicationCase)
+        commandApplicationCasePort.save(applicationCase)
     }
 }

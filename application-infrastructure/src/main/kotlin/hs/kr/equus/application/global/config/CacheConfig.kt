aa11f9bb7cc8ea84117a94ad6275f7f91f77c906
도경 <dokyungkim2926@gmail.com>
2024-05-28 22:26:35 +0900

    add :: redis config

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/config/CacheConfig.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/config/CacheConfig.kt
@@ -1,15 +1,29 @@
 package hs.kr.equus.application.global.config
 
-import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
 
 @Configuration
 @EnableCaching
 class CacheConfig {
+    
     @Bean
-    fun cacheManager(): CacheManager =
-        ConcurrentMapCacheManager()
+    fun redisCacheConfiguration(): RedisCacheConfiguration {
+        return RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofSeconds(60))
+            .disableCachingNullValues()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer<String>(StringRedisSerializer())
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer<Any>(GenericJackson2JsonRedisSerializer())
+            )
+    }
 }

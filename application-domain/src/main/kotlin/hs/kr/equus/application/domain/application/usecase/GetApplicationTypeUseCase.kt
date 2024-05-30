232ffe4c9e0aabb6dcc286b504e1b24f19553112
HyunSu1768 <azxcv1768@gmail.com>
2024-05-30 16:04:47 +0900

    refactor :: 수정

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/GetApplicationTypeUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/GetApplicationTypeUseCase.kt
@@ -1,6 +1,7 @@
 package hs.kr.equus.application.domain.application.usecase
 
 import hs.kr.equus.application.domain.application.exception.ApplicationExceptions
+import hs.kr.equus.application.domain.application.spi.ApplicationQueryGraduationInfoPort
 import hs.kr.equus.application.domain.application.spi.ApplicationQueryUserPort
 import hs.kr.equus.application.domain.application.spi.QueryApplicationPort
 import hs.kr.equus.application.domain.application.usecase.dto.response.GetApplicationTypeResponse
@@ -11,19 +12,23 @@ import hs.kr.equus.application.global.security.spi.SecurityPort
 class GetApplicationTypeUseCase(
     private val queryApplicationPort: QueryApplicationPort,
     private val applicationQueryUserPort: ApplicationQueryUserPort,
-    private val securityPort: SecurityPort
+    private val securityPort: SecurityPort,
+    private val applicationQueryGraduationInfoPort: ApplicationQueryGraduationInfoPort
 ) {
     fun execute(): GetApplicationTypeResponse {
         val userId = securityPort.getCurrentUserId()
         val application = queryApplicationPort.queryApplicationByUserId(userId)
             ?: throw ApplicationExceptions.ApplicationNotFoundException()
 
+        val graduationInfo = applicationQueryGraduationInfoPort.queryGraduationInfoByApplication(application)
+
         return GetApplicationTypeResponse(
             educationalStatus = application.educationalStatus,
             applicationType = application.applicationType,
             isDaejeon = application.isDaejeon,
             applicationRemark = application.applicationRemark,
-            isOutOfHeadCount = application.isOutOfHeadcount
+            isOutOfHeadCount = application.isOutOfHeadcount,
+            graduatedDate = graduationInfo?.graduateDate
         )
     }
 }
--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/response/GetApplicationTypeResponse.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/response/GetApplicationTypeResponse.kt
@@ -3,11 +3,13 @@ package hs.kr.equus.application.domain.application.usecase.dto.response
 import hs.kr.equus.application.domain.application.model.types.ApplicationRemark
 import hs.kr.equus.application.domain.application.model.types.ApplicationType
 import hs.kr.equus.application.domain.application.model.types.EducationalStatus
+import java.time.LocalDate
 
 data class GetApplicationTypeResponse(
     val educationalStatus: EducationalStatus?,
     val applicationType: ApplicationType?,
     val isDaejeon: Boolean?,
     val applicationRemark: ApplicationRemark?,
-    val isOutOfHeadCount: Boolean?
+    val isOutOfHeadCount: Boolean?,
+    val graduatedDate: LocalDate?
 )

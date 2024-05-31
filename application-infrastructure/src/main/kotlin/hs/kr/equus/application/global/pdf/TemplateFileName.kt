e843c3fbca693d08e1ddf60238d67b9bba615b12
도경 <dokyungkim2926@gmail.com>
2024-05-31 19:08:15 +0900

    add :: FileName

---
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/pdf/TemplateFileName.kt
@@ -0,0 +1,10 @@
+package hs.kr.equus.application.global.pdf
+
+object TemplateFileName {
+    const val APPLICATION_FOR_ADMISSION = "/templates/application_for_admission"
+    const val PRIVACY_AGREEMENT = "/templates/privacy_agreement"
+    const val INTRODUCTION = "/templates/introduction"
+    const val NON_SMOKING = "/templates/nonsmoking"
+    const val RECOMMENDATION = "/templates/recommendation"
+    const val SMOKING_EXAMINE = "/templates/smoking_examine"
+}

e93355b358aae30effe6191d8958c33761fdcd16
도경 <dokyungkim2926@gmail.com>
2024-06-03 22:28:39 +0900

    modify :: path

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/util/pdf/TemplateFileName.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/util/pdf/TemplateFileName.kt
@@ -1,10 +1,10 @@
 package hs.kr.equus.application.global.util.pdf
 
 object TemplateFileName {
-    const val APPLICATION_FOR_ADMISSION = "/templates/application_for_admission.html"
-    const val PRIVACY_AGREEMENT = "/templates/privacy_agreement.html"
-    const val INTRODUCTION = "/templates/introduction.html"
-    const val NON_SMOKING = "/templates/nonsmoking.html"
-    const val RECOMMENDATION = "/templates/recommendation.html"
-    const val SMOKING_EXAMINE = "/templates/smoking_examine.html"
+    const val APPLICATION_FOR_ADMISSION = "/application_for_admission.html"
+    const val PRIVACY_AGREEMENT = "/privacy_agreement.html"
+    const val INTRODUCTION = "/introduction.html"
+    const val NON_SMOKING = "/nonsmoking.html"
+    const val RECOMMENDATION = "/recommendation.html"
+    const val SMOKING_EXAMINE = "/smoking_examine.html"
 }

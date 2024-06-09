a50dd69fd742e4a4190ae2056ce294504e1b18fe
도경 <dokyungkim2926@gmail.com>
2024-06-09 16:19:50 +0900

    add :: pdf final api

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/ApplicationPdfUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/ApplicationPdfUseCase.kt
@@ -3,12 +3,16 @@ package hs.kr.equus.application.domain.application.usecase
 import hs.kr.equus.application.domain.application.exception.ApplicationExceptions
 import hs.kr.equus.application.domain.application.model.Application
 import hs.kr.equus.application.domain.application.spi.ApplicationPdfGeneratorPort
+import hs.kr.equus.application.domain.application.spi.ApplicationQueryStatusPort
 import hs.kr.equus.application.domain.application.spi.QueryApplicationPort
 import hs.kr.equus.application.domain.graduationInfo.exception.GraduationInfoExceptions
 import hs.kr.equus.application.domain.score.exception.ScoreExceptions
+import hs.kr.equus.application.domain.score.model.Score
 import hs.kr.equus.application.domain.score.spi.QueryScorePort
+import hs.kr.equus.application.domain.status.exception.StatusExceptions
 import hs.kr.equus.application.global.annotation.ReadOnlyUseCase
 import hs.kr.equus.application.global.security.spi.SecurityPort
+import java.util.UUID
 
 
 @ReadOnlyUseCase
@@ -17,22 +21,48 @@ class ApplicationPdfUseCase(
     private val queryApplicationPort: QueryApplicationPort,
     private val queryScorePort: QueryScorePort,
     private val applicationPdfGeneratorPort: ApplicationPdfGeneratorPort,
+    private val queryStatusPort: ApplicationQueryStatusPort
 ) {
 
-    fun getPreviewApplicationPdf(): ByteArray {
+    fun getFinalApplicationPdf(): ByteArray {
         val userId = securityPort.getCurrentUserId()
-        val application = queryApplicationPort.queryApplicationByUserId(userId)
-            ?: throw ApplicationExceptions.ApplicationNotFoundException()
+        val application = getApplication(userId)
+
+        val status = queryStatusPort.queryStatusByReceiptCode(application.receiptCode)
+            ?: throw StatusExceptions.StatusNotFoundException()
+
+        if (status.isSubmitted) throw StatusExceptions.AlreadySubmittedException()
 
         validatePrintableApplicant(application)
 
-        val calculatedScore = queryScorePort.queryScoreByReceiptCode(application.receiptCode)
-            ?: throw ScoreExceptions.ScoreNotFoundException()
-
-        return applicationPdfGeneratorPort.generate(application, calculatedScore)
+        val calculatedScore = getScore(application.receiptCode)
+        return generatePdf(application, calculatedScore)
     }
 
+    fun getPreviewApplicationPdf(): ByteArray {
+        val userId = securityPort.getCurrentUserId()
+        val application = getApplication(userId)
+
+        validatePrintableApplicant(application)
+
+        val calculatedScore = getScore(application.receiptCode)
+
+        return generatePdf(application, calculatedScore)
+    }
+
+    private fun generatePdf(application: Application, score: Score) =
+        applicationPdfGeneratorPort.generate(application, score)
+
+    private fun getScore(receiptCode: Long) =
+        queryScorePort.queryScoreByReceiptCode(receiptCode)
+            ?: throw ScoreExceptions.ScoreNotFoundException()
+
+    private fun getApplication(userId: UUID) =
+        queryApplicationPort.queryApplicationByUserId(userId)
+            ?: throw ApplicationExceptions.ApplicationNotFoundException()
+
     private fun validatePrintableApplicant(application: Application) {
-        if (application.isEducationalStatusEmpty()) throw GraduationInfoExceptions.EducationalStatusUnmatchedException()
+        if (application.isEducationalStatusEmpty())
+            throw GraduationInfoExceptions.EducationalStatusUnmatchedException()
     }
 }
--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/application/presentation/WepApplicationPdfAdapter.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/application/presentation/WepApplicationPdfAdapter.kt
@@ -5,6 +5,8 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.nio.charset.StandardCharsets
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 @RequestMapping("/pdf")
@@ -13,4 +15,17 @@ class WepApplicationPdfAdapter(
 ) {
     @GetMapping("/preview", produces = [MediaType.APPLICATION_PDF_VALUE])
     fun previewPdf(): ByteArray = applicationPdfUseCase.getPreviewApplicationPdf()
+
+    @GetMapping("/final", produces = [MediaType.APPLICATION_PDF_VALUE])
+    fun finalPdf(response: HttpServletResponse): ByteArray {
+        response.setHeader("Content-Disposition", "attachment; filename=\"${encodeFileName()}.pdf\"")
+        return applicationPdfUseCase.getFinalApplicationPdf()
+    }
+
+    private fun encodeFileName(): String {
+        return String(FILE_NAME.toByteArray(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1)
+    }
+    companion object {
+        const val FILE_NAME = "entry"
+    }
 }

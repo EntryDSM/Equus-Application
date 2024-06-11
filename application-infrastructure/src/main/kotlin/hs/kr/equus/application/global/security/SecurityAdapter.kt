c25257c951158965543424700ed63dad5ef99c05
현수 <azxcv1768@gmail.com>
2024-06-12 08:33:38 +0900

    Update SecurityAdapter.kt

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/security/SecurityAdapter.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/security/SecurityAdapter.kt
@@ -1,13 +1,20 @@
 package hs.kr.equus.application.global.security
 
+import hs.kr.equus.application.global.exception.GlobalExceptions
 import hs.kr.equus.application.global.security.spi.SecurityPort
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import java.lang.IllegalArgumentException
 import java.util.UUID
 
 @Component
 class SecurityAdapter : SecurityPort {
     override fun getCurrentUserId(): UUID {
-        return UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+        val userId = SecurityContextHolder.getContext().authentication.name
+        try {
+            return UUID.fromString(userId)
+        } catch (e: IllegalArgumentException) {
+            throw GlobalExceptions.InvalidTokenException()
+        }
     }
 }

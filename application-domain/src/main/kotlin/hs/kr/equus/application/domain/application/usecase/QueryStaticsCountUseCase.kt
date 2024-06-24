28cced06f5af72e98ee5b06f3ee3d836c1e3baf1
도경 <dokyungkim2926@gmail.com>
2024-06-25 02:13:39 +0900

    문서화

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/QueryStaticsCountUseCase.kt
@@ -0,0 +1,21 @@
+package hs.kr.equus.application.domain.application.usecase
+
+import hs.kr.equus.application.domain.application.model.types.ApplicationType
+import hs.kr.equus.application.domain.application.spi.QueryStaticsCountPort
+import hs.kr.equus.application.domain.application.usecase.dto.response.GetStaticsCountResponse
+import hs.kr.equus.application.global.annotation.ReadOnlyUseCase
+
+@ReadOnlyUseCase
+class QueryStaticsCountUseCase(
+    private val queryStaticsCountPort: QueryStaticsCountPort
+) {
+    fun execute(): List<GetStaticsCountResponse> {
+        return ApplicationType.values().flatMap { it ->
+            // 대전 true false를 나누어 처리한다
+            listOf(false, true).map { isDaejeon ->
+                val count = queryStaticsCountPort.queryStaticsCount(it, isDaejeon).count()
+                GetStaticsCountResponse(it, isDaejeon, count)
+            }
+        }
+    }
+}

19d20f2188d53bc63754011efcded2b6bac2cad7
도경 <dokyungkim2926@gmail.com>
2024-06-27 23:34:30 +0900

    add :: queryTotalScore

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/domain/ScorePersistenceAdapter.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/domain/ScorePersistenceAdapter.kt
@@ -1,16 +1,19 @@
 package hs.kr.equus.application.domain.score.domain
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import hs.kr.equus.application.domain.score.domain.mapper.ScoreMapper
 import hs.kr.equus.application.domain.score.domain.repository.ScoreJpaRepository
 import hs.kr.equus.application.domain.score.model.Score
 import hs.kr.equus.application.domain.score.spi.ScorePort
-import hs.kr.equus.application.domain.status.model.Status
+import hs.kr.equus.application.domain.score.usecase.dto.response.QueryTotalScoreResponse
 import org.springframework.stereotype.Component
+import hs.kr.equus.application.domain.score.domain.entity.QScoreJpaEntity.scoreJpaEntity
 
 @Component
 class ScorePersistenceAdapter(
     private val scoreMapper: ScoreMapper,
     private val scoreJpaRepository: ScoreJpaRepository,
+    private val jpaQueryFactory: JPAQueryFactory
 ) : ScorePort {
     override fun save(score: Score): Score {
         return scoreJpaRepository.save(
@@ -21,5 +24,13 @@ class ScorePersistenceAdapter(
     override fun queryScoreByReceiptCode(receiptCode: Long): Score? {
         return scoreJpaRepository.findByReceiptCode(receiptCode)
             .let(scoreMapper::toDomain)
+    }
+
+    override fun queryTotalScore(receiptCode: Long): QueryTotalScoreResponse {
+        return jpaQueryFactory.select(scoreJpaEntity.totalScore)
+            .from(scoreJpaEntity)
+            .where(scoreJpaEntity.receiptCode.eq(receiptCode))
+            .fetchOne()
+            .let { QueryTotalScoreResponse(it) }
     }
 }

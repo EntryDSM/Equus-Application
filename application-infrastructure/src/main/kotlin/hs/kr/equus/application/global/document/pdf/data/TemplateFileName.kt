abf0345eb2e023fd562755c2c948340214608f47
HyunSu1768 <azxcv1768@gmail.com>
2024-06-27 21:24:37 +0900

    bug :: 버그수정

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/document/pdf/data/TemplateFileName.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/document/pdf/data/TemplateFileName.kt
@@ -1,10 +1,10 @@
 package hs.kr.equus.application.global.document.pdf.data
 
 object TemplateFileName {
-    const val APPLICATION_FOR_ADMISSION = "application_for_admission.html"
-    const val PRIVACY_AGREEMENT = "privacy_agreement.html"
-    const val INTRODUCTION = "introduction.html"
-    const val NON_SMOKING = "nonsmoking.html"
-    const val RECOMMENDATION = "recommendation.html"
-    const val SMOKING_EXAMINE = "smoking_examine.html"
+    const val APPLICATION_FOR_ADMISSION = "/application_for_admission.html"
+    const val PRIVACY_AGREEMENT = "/privacy_agreement.html"
+    const val INTRODUCTION = "/introduction.html"
+    const val NON_SMOKING = "/nonsmoking.html"
+    const val RECOMMENDATION = "/recommendation.html"
+    const val SMOKING_EXAMINE = "/smoking_examine.html"
 }

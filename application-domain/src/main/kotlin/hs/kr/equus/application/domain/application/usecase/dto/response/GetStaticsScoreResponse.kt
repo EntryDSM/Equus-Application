dadbabaf49ee1b8362637c25bfc244aea5524242
도경 <dokyungkim2926@gmail.com>
2024-06-28 22:57:59 +0900

    modify :: response

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/response/GetStaticsScoreResponse.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/response/GetStaticsScoreResponse.kt
@@ -2,8 +2,9 @@ package hs.kr.equus.application.domain.application.usecase.dto.response
 
 import hs.kr.equus.application.domain.application.model.types.ApplicationType
 
-abstract class GetStaticsScoreResponse {
-    protected abstract val applicationType: ApplicationType
-    protected val isDaejeon: Boolean = false
+abstract class GetStaticsScoreResponse(
+    val applicationType: ApplicationType,
+    val isDaejeon: Boolean = false
+) {
     abstract fun addScore(score: Double)
 }

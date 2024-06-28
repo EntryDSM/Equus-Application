abcbb2c82768ba63f6251747cc9fecb4ab782447
rlaisqls <rlaisqls@gmail.com>
2024-06-28 16:07:02 +0900

    bug :: PdfConfig 테스트

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/config/PdfConfig.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/config/PdfConfig.kt
@@ -3,30 +3,19 @@ package hs.kr.equus.application.global.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.thymeleaf.TemplateEngine
-import org.thymeleaf.spring5.SpringTemplateEngine
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver
 import org.thymeleaf.templatemode.TemplateMode
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
 
-//@Configuration
-//class PdfConfig {
-//    @Bean
-//    fun templateEngine(): TemplateEngine {
-//        val templateEngine: TemplateEngine = SpringTemplateEngine()
-//        templateEngine.addTemplateResolver(springResourceTemplateResolver())
-//        return templateEngine
-//    }
-//
-//    @Bean
-//    fun springResourceTemplateResolver(): SpringResourceTemplateResolver {
-//        val springResourceTemplateResolver = SpringResourceTemplateResolver()
-//        springResourceTemplateResolver.order = 1
-//        springResourceTemplateResolver.prefix = "classpath:templates"
-//        springResourceTemplateResolver.suffix = ".html"
-//        springResourceTemplateResolver.templateMode = TemplateMode.HTML
-//        springResourceTemplateResolver.characterEncoding = "UTF-8"
-//        springResourceTemplateResolver.isCacheable = false
-//
-//        return springResourceTemplateResolver
-//    }
-//}
+@Configuration
+class PdfConfig {
+    @Bean
+    fun templateEngine(): TemplateEngine {
+        val templateResolver = ClassLoaderTemplateResolver().apply {
+            prefix = "classpath:/templates"
+            suffix = ".html"
+            templateMode = TemplateMode.HTML
+        }
+        return TemplateEngine().apply { this.setTemplateResolver(templateResolver) }
+    }
+}

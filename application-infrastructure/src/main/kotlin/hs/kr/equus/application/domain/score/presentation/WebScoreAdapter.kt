ff1c6a69ac3b017ee0817d95af063c2ede806089
도경 <dokyungkim2926@gmail.com>
2024-06-30 01:22:19 +0900

    add :: adapter

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/presentation/WebScoreAdapter.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/presentation/WebScoreAdapter.kt
@@ -1,8 +1,19 @@
 package hs.kr.equus.application.domain.score.presentation
 
+import hs.kr.equus.application.domain.score.usecase.QueryStaticsScoreUseCase
+import hs.kr.equus.application.domain.score.usecase.dto.response.GetScoreResposne
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/score")
-class WebScoreAdapter
+class WebScoreAdapter(
+    private val queryStaticsScoreUseCase: QueryStaticsScoreUseCase
+) {
+    @GetMapping
+    fun queryStaticsScore(): List<GetScoreResposne> {
+
+        return queryStaticsScoreUseCase.execute()
+    }
+}

df73411053d87dd58434f92a654dbcc98094c2a0
도경 <dokyungkim2926@gmail.com>
2024-06-30 01:39:00 +0900

    modify :: adapter

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/presentation/WebScoreAdapter.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/presentation/WebScoreAdapter.kt
@@ -1,7 +1,7 @@
 package hs.kr.equus.application.domain.score.presentation
 
+import hs.kr.equus.application.domain.score.presentation.dto.reponse.GetScoreResponse
 import hs.kr.equus.application.domain.score.usecase.QueryStaticsScoreUseCase
-import hs.kr.equus.application.domain.score.usecase.dto.response.GetScoreResposne
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,8 +12,20 @@ class WebScoreAdapter(
     private val queryStaticsScoreUseCase: QueryStaticsScoreUseCase
 ) {
     @GetMapping
-    fun queryStaticsScore(): List<GetScoreResposne> {
-
-        return queryStaticsScoreUseCase.execute()
+    fun queryStaticsScore(): List<GetScoreResponse> {
+        return queryStaticsScoreUseCase.execute().map {
+            GetScoreResponse(
+                applicationType = it.applicationType,
+                isDaejeon = it.isDaejeon,
+                score_79 = it.score_79,
+                score80_92 = it.score80_92,
+                score93_105 = it.score93_105,
+                score106_118 = it.score106_118,
+                score119_131 = it.score119_131,
+                score132_144 = it.score132_144,
+                score145_157 = it.score145_157,
+                score158_170 = it.score158_170
+            )
+        }
     }
 }
--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/presentation/dto/reponse/GetScoreResponse.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/presentation/dto/reponse/GetScoreResponse.kt
@@ -6,12 +6,12 @@ import hs.kr.equus.application.domain.application.model.types.ApplicationType
 data class GetScoreResponse(
     val applicationType: ApplicationType,
     val isDaejeon: Boolean,
-    val score158_170: Int,
-    val score145_157: Int,
-    val score132_144: Int,
-    val score119_131: Int,
-    val score106_118: Int,
-    val score93_105: Int,
-    val score80_92: Int,
-    val score_79: Int
+    @JsonProperty("158-170") val score158_170: Int = 0,
+    @JsonProperty("145-157") val score145_157: Int = 0,
+    @JsonProperty("132-144") val score132_144: Int = 0,
+    @JsonProperty("119-131") val score119_131: Int = 0,
+    @JsonProperty("106-118") val score106_118: Int = 0,
+    @JsonProperty("93-105") val score93_105: Int = 0,
+    @JsonProperty("80-92") val score80_92: Int = 0,
+    @JsonProperty("-79") val score_79: Int = 0
 )

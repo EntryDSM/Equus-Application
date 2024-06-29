ebbc2fa761ee968ad4cc553a272d5a5cf6fff842
도경 <dokyungkim2926@gmail.com>
2024-06-30 01:38:54 +0900

    rename

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/usecase/QueryStaticsScoreUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/usecase/QueryStaticsScoreUseCase.kt
@@ -1,21 +1,20 @@
 package hs.kr.equus.application.domain.score.usecase
 
 import hs.kr.equus.application.domain.application.model.types.ApplicationType
-import hs.kr.equus.application.domain.score.exception.ScoreExceptions
 import hs.kr.equus.application.domain.score.spi.ScoreQueryApplicationTypeAndIsDaejeonPort
-import hs.kr.equus.application.domain.score.usecase.dto.response.GetScoreResposne
+import hs.kr.equus.application.domain.score.usecase.dto.response.GetStaticsScoreResponse
 import hs.kr.equus.application.global.annotation.ReadOnlyUseCase
 
 @ReadOnlyUseCase
 class QueryStaticsScoreUseCase(
     private val scoreQueryApplicationTypeAndIsDaejeonPort: ScoreQueryApplicationTypeAndIsDaejeonPort
 ) {
-    fun execute(): List<GetScoreResposne> {
+    fun execute(): List<GetStaticsScoreResponse> {
         return ApplicationType.values().flatMap { type ->
             listOf(true, false).map { isDaejeon ->
                 val response = when {
-                    type == ApplicationType.COMMON -> GetScoreResposne(isDaejeon, ApplicationType.COMMON)
-                    else -> GetScoreResposne(isDaejeon, type)
+                    type == ApplicationType.COMMON -> GetStaticsScoreResponse(isDaejeon, ApplicationType.COMMON)
+                    else -> GetStaticsScoreResponse(isDaejeon, type)
                 }
                 scoreQueryApplicationTypeAndIsDaejeonPort.queryScoreByApplicationTypeAndIsDaejeon(type, isDaejeon)
                     .forEach { score ->

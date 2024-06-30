98530ac816ebaa4c56805659b2df0a5d77e92e02
도경 <dokyungkim2926@gmail.com>
2024-06-30 21:36:31 +0900

    modify :: usecase

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/usecase/QueryStaticsScoreUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/usecase/QueryStaticsScoreUseCase.kt
@@ -1,6 +1,7 @@
 package hs.kr.equus.application.domain.score.usecase
 
 import hs.kr.equus.application.domain.application.model.types.ApplicationType
+import hs.kr.equus.application.domain.score.service.GetAddScoreService
 import hs.kr.equus.application.domain.score.spi.ApplicationScorePort
 import hs.kr.equus.application.domain.score.spi.ScoreQueryApplicationPort
 import hs.kr.equus.application.domain.score.usecase.dto.response.GetStaticsScoreResponse
@@ -8,21 +9,31 @@ import hs.kr.equus.application.global.annotation.ReadOnlyUseCase
 
 @ReadOnlyUseCase
 class QueryStaticsScoreUseCase(
-    private val applicationScorePort: ApplicationScorePort
+    private val applicationScorePort: ApplicationScorePort,
+    private val getAddScoreService: GetAddScoreService
 ) {
     fun execute(): List<GetStaticsScoreResponse> {
         return ApplicationType.values().flatMap { type ->
             listOf(true, false).map { isDaejeon ->
-                val response = when {
-                    type == ApplicationType.COMMON -> GetStaticsScoreResponse(isDaejeon, ApplicationType.COMMON)
+                val response = when (type) {
+                    ApplicationType.COMMON -> GetStaticsScoreResponse(isDaejeon, ApplicationType.COMMON)
                     else -> GetStaticsScoreResponse(isDaejeon, type)
                 }
-                applicationScorePort.queryScoreByApplicationTypeAndIsDaejeon(type, isDaejeon)
-                    .forEach { score ->
-                        response.addScore(score?.totalScore!!)
-                    }
-                response
+
+                val scores = applicationScorePort.queryScoreByApplicationTypeAndIsDaejeon(type, isDaejeon)
+                scores.mapNotNull { it?.totalScore }.forEach { getAddScoreService.addScore(it) }
+
+                response.apply {
+                    firstRate = getAddScoreService.firstRate
+                    secondRate = getAddScoreService.secondRate
+                    thirdRate = getAddScoreService.thirdRate
+                    fourthRate = getAddScoreService.fourthRate
+                    fifthRate = getAddScoreService.fifthRate
+                    sixthRate = getAddScoreService.sixthRate
+                    seventhRate = getAddScoreService.seventhRate
+                    eighthRate = getAddScoreService.eighthRate
+                }.also { getAddScoreService.resetScore() }
             }
-        }.toList()
+        }
     }
 }

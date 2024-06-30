e21a49b522851fb43084867daba1f18532b8b0fb
도경 <dokyungkim2926@gmail.com>
2024-06-30 20:14:12 +0900

    modify :: usecsae

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/usecase/QueryStaticsScoreUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/usecase/QueryStaticsScoreUseCase.kt
@@ -1,13 +1,14 @@
 package hs.kr.equus.application.domain.score.usecase
 
 import hs.kr.equus.application.domain.application.model.types.ApplicationType
-import hs.kr.equus.application.domain.score.spi.ScoreQueryApplicationTypeAndIsDaejeonPort
+import hs.kr.equus.application.domain.score.spi.ApplicationScorePort
+import hs.kr.equus.application.domain.score.spi.ScoreQueryApplicationPort
 import hs.kr.equus.application.domain.score.usecase.dto.response.GetStaticsScoreResponse
 import hs.kr.equus.application.global.annotation.ReadOnlyUseCase
 
 @ReadOnlyUseCase
 class QueryStaticsScoreUseCase(
-    private val scoreQueryApplicationTypeAndIsDaejeonPort: ScoreQueryApplicationTypeAndIsDaejeonPort
+    private val applicationScorePort: ApplicationScorePort
 ) {
     fun execute(): List<GetStaticsScoreResponse> {
         return ApplicationType.values().flatMap { type ->
@@ -16,9 +17,9 @@ class QueryStaticsScoreUseCase(
                     type == ApplicationType.COMMON -> GetStaticsScoreResponse(isDaejeon, ApplicationType.COMMON)
                     else -> GetStaticsScoreResponse(isDaejeon, type)
                 }
-                scoreQueryApplicationTypeAndIsDaejeonPort.queryScoreByApplicationTypeAndIsDaejeon(type, isDaejeon)
+                applicationScorePort.queryScoreByApplicationTypeAndIsDaejeon(type, isDaejeon)
                     .forEach { score ->
-                        response.addScore(score!!.totalScore!!.toDouble())
+                        response.addScore(score?.totalScore!!)
                     }
                 response
             }

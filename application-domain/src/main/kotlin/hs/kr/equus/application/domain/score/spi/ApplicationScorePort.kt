c64c28ed05c8ace31dd98aac345d9c222efe27d4
도경 <dokyungkim2926@gmail.com>
2024-07-02 16:46:52 +0900

    delete :: application score port

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/spi/ApplicationScorePort.kt
+++
@@ -1,8 +0,0 @@
-package hs.kr.equus.application.domain.score.spi
-
-import hs.kr.equus.application.domain.application.model.types.ApplicationType
-import hs.kr.equus.application.domain.score.model.Score
-
-interface ApplicationScorePort {
-    fun queryScoreByApplicationTypeAndIsDaejeon(applicationType: ApplicationType, isDaejeon: Boolean): List<Score?>
-}

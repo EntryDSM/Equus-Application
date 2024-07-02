006c209e45f77ab6e087acd6e9ef4e0cb8f0f42a
도경 <dokyungkim2926@gmail.com>
2024-07-02 21:36:21 +0900

    modify :: name

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/service/ScoreRatingCalculatorService.kt
@@ -0,0 +1,26 @@
+package hs.kr.equus.application.domain.score.service
+
+import hs.kr.equus.application.global.annotation.DomainService
+import java.math.BigDecimal
+
+@DomainService
+class ScoreRatingCalculatorService {
+    fun rating(scores: List<BigDecimal>): Array<Int> {
+        val ratingCounts = Array(8) { 0 }
+        scores.map {
+            score ->
+            when {
+                score < BigDecimal("80") -> ratingCounts[7]++
+                score < BigDecimal("93") -> ratingCounts[6]++
+                score < BigDecimal("106") -> ratingCounts[5]++
+                score < BigDecimal("119") -> ratingCounts[4]++
+                score < BigDecimal("132") -> ratingCounts[3]++
+                score < BigDecimal("145") -> ratingCounts[2]++
+                score < BigDecimal("158") -> ratingCounts[1]++
+                score <= BigDecimal("170") -> ratingCounts[0]++
+                else -> 0 // 범위 넘어가는 숫자는 0 반환
+            }
+        }
+        return ratingCounts
+    }
+}
--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/usecase/QueryStaticsScoreUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/score/usecase/QueryStaticsScoreUseCase.kt
@@ -1,7 +1,7 @@
 package hs.kr.equus.application.domain.score.usecase
 
 import hs.kr.equus.application.domain.application.model.types.ApplicationType
-import hs.kr.equus.application.domain.score.service.ScoreCalculatorService
+import hs.kr.equus.application.domain.score.service.ScoreRatingCalculatorService
 import hs.kr.equus.application.domain.score.spi.QueryScorePort
 import hs.kr.equus.application.domain.score.usecase.dto.response.GetStaticsScoreResponse
 import hs.kr.equus.application.global.annotation.ReadOnlyUseCase
@@ -9,26 +9,26 @@ import hs.kr.equus.application.global.annotation.ReadOnlyUseCase
 @ReadOnlyUseCase
 class QueryStaticsScoreUseCase(
     private val queryScorePort: QueryScorePort,
-    private val scoreCalculatorService: ScoreCalculatorService
+    private val scoreRatingCalculatorService: ScoreRatingCalculatorService
 ) {
     fun execute(): List<GetStaticsScoreResponse> {
         return ApplicationType.values().flatMap { type ->
             listOf(true, false).map { isDaejeon ->
                 val totalScores = queryScorePort.queryScoreByApplicationTypeAndIsDaejeon(type, isDaejeon)
                     .map { it?.totalScore!! }
-                    .let { scoreCalculatorService.ratingCalculator(it) }
+                    .let { scoreRatingCalculatorService.rating(it) }
 
                 GetStaticsScoreResponse(
                     isDaejeon = isDaejeon,
                     applicationType = type,
-                    firstRate = totalScores[0].toInt(),
-                    secondRate = totalScores[1].toInt(),
-                    thirdRate = totalScores[2].toInt(),
-                    fourthRate = totalScores[3].toInt(),
-                    fifthRate = totalScores[4].toInt(),
-                    sixthRate = totalScores[5].toInt(),
-                    seventhRate = totalScores[6].toInt(),
-                    eighthRate = totalScores[7].toInt()
+                    firstRate = totalScores[0],
+                    secondRate = totalScores[1],
+                    thirdRate = totalScores[2],
+                    fourthRate = totalScores[3],
+                    fifthRate = totalScores[4],
+                    sixthRate = totalScores[5],
+                    seventhRate = totalScores[6],
+                    eighthRate = totalScores[7]
                 )
             }
         }

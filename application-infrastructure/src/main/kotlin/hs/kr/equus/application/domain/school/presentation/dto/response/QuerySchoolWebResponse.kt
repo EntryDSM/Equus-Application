dfa2e818c5d4d0301f322da8380b0e6a237d5864
도경 <dokyungkim2926@gmail.com>
2024-07-06 23:59:44 +0900

    modify :: jsonproperty -> null

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/school/presentation/dto/response/QuerySchoolWebResponse.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/school/presentation/dto/response/QuerySchoolWebResponse.kt
@@ -1,7 +1,5 @@
 package hs.kr.equus.application.domain.school.presentation.dto.response
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class QuerySchoolWebResponse (
-  @JsonProperty("content") val content: List<SchoolWebResponse>
+  val content: List<SchoolWebResponse> ? = null
 )
--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/school/presentation/dto/response/SchoolWebResponse.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/school/presentation/dto/response/SchoolWebResponse.kt
@@ -1,10 +1,8 @@
 package hs.kr.equus.application.domain.school.presentation.dto.response
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class SchoolWebResponse(
-    @JsonProperty("code") val code: String,
-    @JsonProperty("name") val name: String,
-    @JsonProperty("information") val information: String,
-    @JsonProperty("address") val address: String
+     val code: String? = null,
+     val name: String? = null,
+     val information: String? = null,
+     val address: String? = null
 )

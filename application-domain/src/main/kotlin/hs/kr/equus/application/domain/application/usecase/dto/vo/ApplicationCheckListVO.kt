f5acadaa7c13d2dd29034ac963968e30fb44e826
도경 <dokyungkim2926@gmail.com>
2024-08-12 23:17:32 +0900

    modify :: vo

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/vo/ApplicationCheckListVO.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/vo/ApplicationCheckListVO.kt
@@ -24,16 +24,27 @@ data class ApplicationCheckListVO (
     val classNumber: String,
     val parentName: String,
     val parentTel: String,
-    val koreanGrade: String,
-    val socialGrade: String,
-    val historyGrade: String,
-    val mathGrade: String,
-    val scienceGrade: String,
-    val englishGrade: String,
-    val techAndHomeGrade: String,
+    val koreanGrade: String = "XXXX",
+    val socialGrade: String = "XXXX",
+    val historyGrade: String = "XXXX",
+    val mathGrade: String = "XXXX",
+    val scienceGrade: String = "XXXX",
+    val englishGrade: String = "XXXX",
+    val techAndHomeGrade: String = "XXXX",
     val latenessCount: Int,
     val earlyLeaveCount: Int,
     val extraScoreItem: ExtraScoreItem,
     val absenceDayCount: Int,
     val lectureAbsenceCount: Int,
+    val totalScore3rdYear: Int,  // 총합 점수: 3학년 성적 총합
+    val lastSemesterTotal: Int,  // 직전 학기 성적 총합
+    val beforeLastSemesterTotal: Int,  // 직전전 학기 성적 총합
+    val convertedScore: Int,  // 교과성적환산점수
+    val volunteerHours: Int,  // 봉사시간
+    val volunteerScore: Int,  // 봉사점수
+    val result: String,  // 결과
+    val attendanceScore: Int,  // 출석점수
+    val competition: String,  // 대회
+    val certification: String,
+    val totalScore1stSelection: Int
 )

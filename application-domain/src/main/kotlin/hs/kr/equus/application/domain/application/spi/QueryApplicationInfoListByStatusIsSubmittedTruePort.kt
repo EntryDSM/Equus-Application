0e221ad0fc27f41e23cec143577d603ef0b00e95
도경 <dokyungkim2926@gmail.com>
2024-08-13 12:04:38 +0900

    add :: port

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/spi/QueryApplicationInfoListByStatusIsSubmittedTruePort.kt
@@ -0,0 +1,7 @@
+package hs.kr.equus.application.domain.application.spi
+
+import hs.kr.equus.application.domain.application.usecase.dto.vo.ApplicationInfoVO
+
+interface QueryApplicationInfoListByStatusIsSubmittedTruePort {
+    fun queryApplicationInfoListByStatusIsSubmittedTrue(): List<ApplicationInfoVO>
+}

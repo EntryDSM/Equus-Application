dd3a32e22676c7c793f93895f1d80c6ff2947510
도경 <dokyungkim2926@gmail.com>
2024-08-13 12:02:07 +0900

    add :: ApplicationInfoVO

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/vo/ApplicationInfoVO.kt
@@ -0,0 +1,15 @@
+package hs.kr.equus.application.domain.application.usecase.dto.vo
+
+import hs.kr.equus.application.domain.application.model.Application
+import hs.kr.equus.application.domain.applicationCase.model.GraduationCase
+import hs.kr.equus.application.domain.graduationInfo.model.Graduation
+import hs.kr.equus.application.domain.score.model.Score
+import hs.kr.equus.application.domain.status.model.Status
+
+data class ApplicationInfoVO (
+    val application: Application,
+    val graduation: Graduation,
+    val graduationCase: GraduationCase,
+    val status: Status,
+    val score: Score
+)

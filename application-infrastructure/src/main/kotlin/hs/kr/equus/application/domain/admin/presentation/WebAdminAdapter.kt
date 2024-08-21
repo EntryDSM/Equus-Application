644b08a897c37023d251b434e052980a215224c3
HyunSu1768 <azxcv1768@gmail.com>
2024-08-21 23:07:11 +0900

    Swap offset and pageSize parameters order

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/admin/presentation/WebAdminAdapter.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/admin/presentation/WebAdminAdapter.kt
@@ -66,9 +66,9 @@ class WebAdminAdapter(
 
     @GetMapping("/applicants")
     fun getApplicants(
-        @RequestParam(name = "offset", defaultValue = "0")
-        pageSize: Long,
         @RequestParam(name = "pageSize", defaultValue = "10")
+        pageSize: Long,
+        @RequestParam(name = "offset", defaultValue = "0")
         offset: Long,
         @ModelAttribute
         getApplicantsRequest: GetApplicantsRequest

fd76bfb9eef20e34c78295c90639d64a05c2f22d
HyunSu1768 <azxcv1768@gmail.com>
2024-08-23 01:48:16 +0900

    Update default values in GetApplicantsRequest data class

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/request/GetApplicantsRequest.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/request/GetApplicantsRequest.kt
@@ -1,24 +1,32 @@
 package hs.kr.equus.application.domain.application.usecase.dto.request
 
-import hs.kr.equus.application.domain.application.exception.ApplicationExceptions
-
 data class GetApplicantsRequest(
     val name: String? = "",
     val schoolName: String? = "",
-    val isDaejeon: Boolean? = null,
-    val isNationwide: Boolean? = null,
+    var isDaejeon: Boolean = false,
+    var isNationwide: Boolean = false,
     var isCommon: Boolean = false,
     var isMeister: Boolean = false,
     var isSocial: Boolean = false,
-    val isOutOfHeadcount: Boolean? = null,
-    val isSubmitted: Boolean? = null,
-    val isNotSubmitted: Boolean? = null
+    val isOutOfHeadcount: Boolean = false,
+    var isSubmitted: Boolean = false,
+    var isNotSubmitted: Boolean = false
 ) {
     init {
         if (!isCommon && !isMeister && !isSocial) {
             isCommon = true
             isMeister = true
             isSocial = true
+        }
+
+        if(isSubmitted && isNotSubmitted) {
+            isSubmitted = true
+            isNotSubmitted = true
+        }
+
+        if(isDaejeon && isNationwide) {
+            isDaejeon = true
+            isNationwide = true
         }
     }
 }

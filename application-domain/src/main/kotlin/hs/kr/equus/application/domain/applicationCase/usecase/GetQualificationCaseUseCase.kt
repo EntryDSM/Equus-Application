543384bc99000addbc500ec85bca29601606bab8
도경 <dokyungkim2926@gmail.com>
2024-09-01 01:35:06 +0900

    line

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/applicationCase/usecase/GetQualificationCaseUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/applicationCase/usecase/GetQualificationCaseUseCase.kt
@@ -24,7 +24,7 @@ class GetQualificationCaseUseCase(
 
         val qualificationCase = queryApplicationCasePort.queryApplicationCaseByApplication(application)
 
-        if(qualificationCase !is QualificationCase) throw ApplicationCaseExceptions.EducationalStatusUnmatchedException()
+        if (qualificationCase !is QualificationCase) throw ApplicationCaseExceptions.EducationalStatusUnmatchedException()
 
         return GetQualificationCaseResponse(
             koreanGrade = qualificationCase.koreanGrade,

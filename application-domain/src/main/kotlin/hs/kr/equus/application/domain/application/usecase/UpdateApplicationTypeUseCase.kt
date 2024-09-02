c58c137a71dd95deb5cbdd1c2fd76ae49d37b519
도경 <dokyungkim2926@gmail.com>
2024-09-02 12:29:13 +0900

    modify :: application remark

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/UpdateApplicationTypeUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/UpdateApplicationTypeUseCase.kt
@@ -1,6 +1,7 @@
 package hs.kr.equus.application.domain.application.usecase
 
 import hs.kr.equus.application.domain.application.exception.ApplicationExceptions
+import hs.kr.equus.application.domain.application.model.types.ApplicationRemark
 import hs.kr.equus.application.domain.application.spi.CommandApplicationPort
 import hs.kr.equus.application.domain.application.spi.QueryApplicationPort
 import hs.kr.equus.application.domain.application.usecase.dto.request.UpdateApplicationTypeRequest
@@ -22,7 +23,7 @@ class UpdateApplicationTypeUseCase(
             commandApplicationPort.save(
                 application.copy(
                     applicationType = applicationType,
-                    applicationRemark = applicationRemark,
+                    applicationRemark = applicationRemark ?: ApplicationRemark.NONE,
                     isDaejeon = isDaejeon,
                     isOutOfHeadcount = isOutOfHeadcount,
                     veteransNumber = veteransNumber

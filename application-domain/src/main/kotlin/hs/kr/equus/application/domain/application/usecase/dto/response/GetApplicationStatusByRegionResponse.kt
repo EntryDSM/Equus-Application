baef91e940b391f0c63b8728ce7b1b493c2787ec
도경 <dokyungkim2926@gmail.com>
2024-09-11 19:41:41 +0900

    add :: response

---
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/dto/response/GetApplicationStatusByRegionResponse.kt
@@ -0,0 +1,28 @@
+package hs.kr.equus.application.domain.application.usecase.dto.response
+
+data class GetApplicationStatusByRegionResponse (
+    val regionList: List<RegionList>,
+)
+
+data class RegionList(
+    val gangwonDo: Int = 0,
+    val gyeonggiDo: Int = 0,
+    val chungcheongnamDo: Int = 0,
+    val chungcheongbukDo: Int = 0,
+    val jeollanamDo: Int = 0,
+    val jeollabukDo: Int = 0,
+    val gyeongsangbukDo: Int = 0,
+    val gyeongsangnamDo: Int = 0,
+    val seoul: Int = 0,
+    val incheon: Int = 0,
+    val daejeon: Int = 0,
+    val gwangju: Int = 0,
+    val daegu: Int = 0,
+    val busan: Int = 0,
+    val sejong: Int = 0,
+    val ulsan: Int = 0,
+    val jeju: Int = 0,
+)
+
+
+

ac5b769f3dec65b17c8712163065e7ee567ad022
도경 <dokyungkim2926@gmail.com>
2024-09-19 19:33:17 +0900

    delte :: print

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/PrintAdmissionTicketUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/PrintAdmissionTicketUseCase.kt
@@ -35,7 +35,6 @@ class PrintAdmissionTicketUseCase(
                 )
             }
 
-        println(applications)
         printAdmissionTicketPort.execute(httpServletResponse, applications)
     }
 }

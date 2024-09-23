5c62f6448b5ac106a38285832fa943b81fce731e
도경 <dokyungkim2926@gmail.com>
2024-09-23 09:01:33 +0900

    refactoring

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/event/CreateApplicationStatusRollbackConsumer.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/score/event/CreateApplicationStatusRollbackConsumer.kt
@@ -7,7 +7,7 @@ import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
-class CreateApplicatonStatusRollbackConsumer(
+class CreateApplicationStatusRollbackConsumer(
     private val objectMapper: ObjectMapper,
     private val deleteScoreUseCase: DeleteScoreUseCase
 ) {

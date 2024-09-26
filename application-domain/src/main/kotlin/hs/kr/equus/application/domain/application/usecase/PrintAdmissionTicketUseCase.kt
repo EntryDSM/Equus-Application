520e501aea9ff6e4aae81a1cfceb7dcc552f59fd
HyunSu1768 <azxcv1768@gmail.com>
2024-09-27 08:41:22 +0900

    fix(PrintAdmissionTicketUseCase.kt): handle cases for Graduation and Qualification types to improve application ticket printing logic refactor(PrintAdmissionTicketUseCase.kt): restructure application info retrieval to enhance readability and error handling

--- application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/PrintAdmissionTicketUseCase.kt
+++ application-domain/src/main/kotlin/hs/kr/equus/application/domain/application/usecase/PrintAdmissionTicketUseCase.kt
@@ -2,8 +2,11 @@ package hs.kr.equus.application.domain.application.usecase
 
 import hs.kr.equus.application.domain.application.spi.*
 import hs.kr.equus.application.domain.application.usecase.dto.vo.ApplicationInfoVO
+import hs.kr.equus.application.domain.applicationCase.exception.ApplicationCaseExceptions
 import hs.kr.equus.application.domain.applicationCase.model.GraduationCase
+import hs.kr.equus.application.domain.graduationInfo.exception.GraduationInfoExceptions
 import hs.kr.equus.application.domain.graduationInfo.model.Graduation
+import hs.kr.equus.application.domain.graduationInfo.model.Qualification
 import hs.kr.equus.application.global.annotation.ReadOnlyUseCase
 import javax.servlet.http.HttpServletResponse
 
@@ -21,18 +24,39 @@ class PrintAdmissionTicketUseCase(
         val applications = queryApplicationInfoListByStatusIsSubmittedPort.queryApplicationInfoListByStatusIsSubmitted(true)
             .map { it ->
                 val graduationInfo = queryGraduationInfoPort.queryGraduationInfoByApplication(it)
-                val graduation = graduationInfo as? Graduation
-                val applicationCase = queryApplicationCasePort.queryApplicationCaseByApplication(it)
-                val graduationCase = applicationCase as? GraduationCase
-                val application = queryApplicationPort.queryApplicationByReceiptCode(it.receiptCode)
-                val school = applicationQuerySchoolPort.querySchoolBySchoolCode(graduation!!.schoolCode.toString())
-                ApplicationInfoVO(
-                    application!!,
-                    graduation,
-                    graduationCase,
-                    queryScorePort.queryScoreByReceiptCode(application.receiptCode),
-                    school
-                )
+                    ?: throw GraduationInfoExceptions.GraduationNotFoundException()
+                val result = when (graduationInfo) {
+                    is Graduation -> {
+                        val applicationCase = queryApplicationCasePort.queryApplicationCaseByApplication(it)
+                            ?: throw ApplicationCaseExceptions.ApplicationCaseNotFoundException()
+                        val graduationCase = applicationCase as GraduationCase
+                        val application = queryApplicationPort.queryApplicationByReceiptCode(it.receiptCode)
+                        val school =
+                            applicationQuerySchoolPort.querySchoolBySchoolCode(graduationInfo.schoolCode.toString())
+                        ApplicationInfoVO(
+                            application!!,
+                            graduationInfo,
+                            graduationCase,
+                            queryScorePort.queryScoreByReceiptCode(application.receiptCode),
+                            school
+                        )
+                    }
+
+                    is Qualification -> {
+                        val applicationCase = queryApplicationCasePort.queryApplicationCaseByApplication(it)
+                            ?: throw ApplicationCaseExceptions.ApplicationCaseNotFoundException()
+                        val graduationCase = applicationCase as GraduationCase
+                        val application = queryApplicationPort.queryApplicationByReceiptCode(it.receiptCode)
+                        ApplicationInfoVO(
+                            application!!,
+                            graduationInfo,
+                            graduationCase,
+                            queryScorePort.queryScoreByReceiptCode(application.receiptCode),
+                            null
+                        )
+                    }
+                }
+                result
             }
 
         printAdmissionTicketPort.execute(httpServletResponse, applications)

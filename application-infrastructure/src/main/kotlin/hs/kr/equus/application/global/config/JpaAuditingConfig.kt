66dab7195dac622f99bdf84b6e1ddc38abaf31c6
HyunSu1768 <azxcv1768@gmail.com>
2024-09-26 20:28:47 +0900

    feat(JpaAuditingConfig): add JPA auditing configuration to enable automatic auditing of entity changes

---
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/config/JpaAuditingConfig.kt
@@ -0,0 +1,8 @@
+package hs.kr.equus.application.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfig

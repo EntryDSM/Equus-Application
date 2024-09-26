a84dd9f418a574e840dbbb7f86799f0ef5deecb6
HyunSu1768 <azxcv1768@gmail.com>
2024-09-26 16:05:54 +0900

    feat(application): add deleteByReceiptCode method to CommandApplicationPort interface and implement it in ApplicationPersistenceAdapter feat(application): create DeleteApplicationConsumer to handle delete events from Kafka and invoke deleteByReceiptCode method

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/application/event/DeleteApplicationConsumer.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/domain/application/event/DeleteApplicationConsumer.kt
@@ -1,4 +1,25 @@
 package hs.kr.equus.application.domain.application.event
 
-class DeleteApplicationConsumer {
+import com.fasterxml.jackson.databind.ObjectMapper
+import hs.kr.equus.application.domain.application.event.dto.CreateApplicationEvent
+import hs.kr.equus.application.domain.application.spi.CommandApplicationPort
+import hs.kr.equus.application.global.kafka.config.KafkaTopics
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+@Component
+class DeleteApplicationConsumer(
+    private val mapper: ObjectMapper,
+    private val commandApplicationPort: CommandApplicationPort
+) {
+    @KafkaListener(
+        topics = [KafkaTopics.DELETE_USER],
+        groupId = "delete-application",
+        containerFactory = "kafkaListenerContainerFactory",
+    )
+    fun deleteApplication(message: String) {
+        val receiptCode = mapper.readValue(message, Long::class.java)
+        commandApplicationPort.deleteByReceiptCode(receiptCode)
+    }
 }

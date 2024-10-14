6682a1dc00d4b01e2b14ff2656c064944dd11450
HyunSu1768 <azxcv1768@gmail.com>
2024-10-14 09:30:36 +0900

    chore(build.gradle.kts): add mavenCentral repository to dependencies for better package management feat(RedisConfig.kt): register JavaTimeModule with ObjectMapper to support Java 8 date/time types in JSON serialization and deserialization

--- application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/config/RedisConfig.kt
+++ application-infrastructure/src/main/kotlin/hs/kr/equus/application/global/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -29,6 +30,7 @@ class RedisConfig {
     fun objectMapper(): ObjectMapper {
         val mapper = ObjectMapper()
         mapper.registerModule(KotlinModule())
+        mapper.registerModules(JavaTimeModule())
         mapper.activateDefaultTyping(LaissezFaireSubTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY)
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         return mapper

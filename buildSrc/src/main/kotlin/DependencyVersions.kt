d634718ddb6729b1ccd8ff84a7b16fab2c0c4c46
HyunSu1768 <azxcv1768@gmail.com>
2024-10-17 09:37:55 +0900

    fix(DependencyVersions.kt): update POI_VERSION from 5.3.0 to 5.2.3 to resolve compatibility issues with other dependencies

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -13,7 +13,7 @@ object DependencyVersions {
     const val QUERYDSL = "5.0.0"
     const val BYTE_BUDDY = "1.12.18"
     const val COMMONS_IO = "2.11.0"
-    const val POI_VERSION = "5.3.0"
+    const val POI_VERSION = "5.2.3"
     const val SENTRY_VERSION = "6.10.0"
     const val PDF_ITEXT = "7.2.0"
     const val PDF_HTML = "3.0.3"
